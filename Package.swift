8201f70054cac9943d1e2383db76ccee409bd8be
Vitaliy Zhukov <vi.zhukov@tinkoff.ru>
2022-07-07 14:36:34 +0700

    depenpency

--- Package.swift
+++ Package.swift
@@ -26,7 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TDLibKit",
-            dependencies: ["TDLibFramework"]),
+            dependencies: ["TDLibFramework",
+                           "libc++",
+                           "libz"]),
     ]
 )
 
