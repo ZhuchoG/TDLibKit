b09cf7abfea31efbb5ce9b7e6e5300d64967ce48
Vitaliy Zhukov <vi.zhukov@tinkoff.ru>
2022-09-09 17:36:09 +0700

    Revert "depenpency"
    This reverts commit 8201f70054cac9943d1e2383db76ccee409bd8be.

--- Package.swift
+++ Package.swift
@@ -26,9 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TDLibKit",
-            dependencies: ["TDLibFramework",
-                           "libc++",
-                           "libz"]),
+            dependencies: ["TDLibFramework"]),
     ]
 )
 
