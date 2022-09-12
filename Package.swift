2d241945f06203236f102a1238aab498b73ae8d1
Vitaliy Zhukov <vi.zhukov@tinkoff.ru>
2022-09-12 17:03:18 +0700

    fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ZhuchoG/TDLibFramework", .exact("1.0.0")),
+        .package(url: "https://github.com/ZhuchoG/TDLibFramework", .exact("1.8.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
